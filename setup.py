087d96c0cc2c513f53b4edc47ad0b30698804241
Tommy Chien <ad@tommychien.com>
2024-09-13 16:29:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='memorag',
-    version='0.1.4',
-    description='A Python package for memory-augmented retrieval-augmented generation',
-    author='Tommy Chien',
-    author_email='tommy@chien.io',
+    name="memorag",
+    version="0.1.4",
+    description="A Python package for memory-augmented retrieval-augmented generation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tommy Chien",
+    author_email="tommy@chien.io",
     packages=find_packages(),
-    install_requires=requirements,  
+    install_requires=requirements,
     classifiers=[
-        'Programming Language :: Python :: 3.10',  
-        'Programming Language :: Python :: 3.11',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',  
+    python_requires=">=3.10",
 )
